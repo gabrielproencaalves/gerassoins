13ed8e4158c564242e870e8ebeb2cce95d124518
Gabriel Proença Alves <93501005+gabrielproencaalves@users.noreply.github.com>
2025-03-02 22:45:26 -0300

    Add fraction addition case to resolver_exp

--- main.py
+++ main.py
@@ -267,6 +267,22 @@ def resolver_exp(expr):
             tmp_opdos[0] = fracao(tmp_opdos[0])
             tmp_opdos[1] = fracao(tmp_opdos[1])
 
+            if   e_adicao(expr):
+                resultado.opdos = [
+                  e.exp(e.VALOR, [
+                    tmp_opdos[0].opdos[0].opdos[0]
+                    * tmp_opdos[1].opdos[1].opdos[0]
+                    + tmp_opdos[1].opdos[0].opdos[0]
+                    * tmp_opdos[0].opdos[1].opdos[0],
+                    0
+                  ]),
+                  e.exp(e.VALOR, [
+                    tmp_opdos[0].opdos[1].opdos[0]
+                    * tmp_opdos[1].opdos[1].opdos[0],
+                    0
+                  ])
+                ]
+
 
             resultado.opdos[0] = tmp_opdos[0].opdos[0] + tmp_opdos[1].opdos[0]
         if e_subtracao(expr):
